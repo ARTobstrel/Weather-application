1c4732c2965cf9dd775ae28b3f4a7479d617c882
artem <artemfap@yandex.ru>
2019-09-24 00:50:10 +0300

    'state'

--- src/App.js
+++ src/App.js
@@ -4,26 +4,77 @@ import Form from './components/form'
 import Weather from './components/weather'
 
 const API_KEY = "82b797b6ebc625032318e16f1b42c016";
+
 // const API_KEY2 = "70e1ed322b02acbc57d443dd91065f3e";
 
 
 class App extends React.Component {
 
+    state = {
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunset: undefined,
+        error: undefined
+    };
+
+
     gettingWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
-        const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-        const data = await api_url.json();
-        console.log(data)
+
+        if (city) {
+            const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            const data = await api_url.json();
+            console.log(data);
+
+            if (data.cod == "200") {
+                let sunset = data.sys.sunset;
+                let date = new Date();
+                date.setTime(sunset);
+                let sunset_date = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+                this.setState({
+                    temp: data.main.temp,
+                    city: data.name,
+                    country: data.sys.country,
+                    pressure: data.main.pressure,
+                    sunset: sunset_date,
+                    error: undefined
+                });
+                console.log(this.state);
+                // console.log(sunset_date)
+            } else {
+                this.setState({
+                    temp: undefined,
+                    city: undefined,
+                    country: undefined,
+                    pressure: undefined,
+                    sunset: undefined,
+                    error: "Неправильный город."
+                })
+            }
+        } else {
+            this.setState({
+                temp: undefined,
+                city: undefined,
+                country: undefined,
+                pressure: undefined,
+                sunset: undefined,
+                error: "Введите название города."
+            })
+        }
+
     };
 
     render() {
         return (
             <main>
-                <Info />
-                <Info />
-                <Form weatherMethod={this.gettingWeather} />
-                <Weather />
+                <Info/>
+                <Info/>
+                <Form weatherMethod={this.gettingWeather}/>
+                <Weather weatherState={this.state}/>
             </main>
         )
     }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-class Weather extends React.Component {
-    render() {
-        return (
-            <div>
-                Weather
-            </div>
-        )
-    }
-}
+const Weather = (props) => {
+    return (
+        <div>
+            {props.weatherState.city &&
+                <div>
+                    <p>Местоположение: {props.weatherState.city}, {props.weatherState.country}</p>
+                    <p>Температура: {props.weatherState.temp}</p>
+                    <p>Давление: {props.weatherState.pressure}</p>
+                    <p>Заход солнца: {props.weatherState.sunset}</p>
+                </div>
+            }
+            <p>{props.weatherState.error}</p>
+        </div>
+    )
+};
 
-export default Weather
+// class Weather extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 {this.props.name &&
+//                     <p>City: {this.props.name}</p>
+//                 }
+//                 <p>{this.props.error}</p>
+//             </div>
+//         )
+//     }
+// }
+
+export default Weather;
